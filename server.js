d90c77f2ad6e2c08ab2fbc5cfcdd82c1c7bd58ab
JacksonCaa <truongquocnhat26907@gmail.com>
2026-06-01 10:35:03 +0700

    fix smtp port

--- server.js
+++ server.js
@@ -43,10 +43,15 @@ initDB();
 
 // ==================== EMAIL ====================
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
