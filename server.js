78d9b6f3ad24b288136d1c5c097686e264f9a8bf
JacksonCaa <truongquocnhat26907@gmail.com>
2026-06-04 09:52:57 +0700

    updateseverlan2

--- server.js
+++ server.js
@@ -246,6 +246,6 @@ app.delete('/api/admin/users/:email', async (req, res) => {
 
 // ==================== START ====================
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server chạy tại http://localhost:${PORT}`);
 });
